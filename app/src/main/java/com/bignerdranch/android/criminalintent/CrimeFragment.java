fe804baea0c02e9c5efe5de31fcaaa6ecbd02443
Darius Davedane <denesd93@gmail.com>
2021-12-10 22:06:42 +0100

    Finished picking suspect from Contacts.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,7 +1,11 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.provider.ContactsContract;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -13,6 +17,10 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultCaller;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -20,7 +28,9 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.text.format.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -28,6 +38,13 @@ public class CrimeFragment extends Fragment {
 
     //region Fields
 
+    public final static String REQUEST_DATE = "request_date";
+    public final static String REQUEST_TIME = "request_time";
+    public final static int REQUEST_CONTACT = 1;
+    private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DATE_FORMAT = "EEE, MMM, dd";
+    private final String DIALOG_DATE = "DialogDate";
+    private final String DIALOG_TIME = "DialogTime";
     private Crime crime;
     private EditText titleField;
     private Button dateButton;
@@ -35,17 +52,22 @@ public class CrimeFragment extends Fragment {
     private CheckBox solvedCheckBox;
     private CheckBox requiresPoliceCheckBox;
     private Button reportButton;
-    private final String DIALOG_DATE = "DialogDate";
-    private final String DIALOG_TIME = "DialogTime";
-    public final static String REQUEST_DATE = "request_date";
-    public final static String REQUEST_TIME = "request_time";
-    private static final String ARG_CRIME_ID = "crime_id";
-    private static final String DATE_FORMAT = "EEE, MMM, dd";
+    private Button suspectButton;
     private CrimeDetailViewModel crimeDetailViewModel;
+    ActivityResultLauncher<Void> contactResultLauncher;
+    Intent pickContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
 
     //endregion
 
     //region Lifecycle events
+
+    public static CrimeFragment newInstance(UUID crimeID) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(ARG_CRIME_ID, crimeID);
+        CrimeFragment crimeFragment = new CrimeFragment();
+        crimeFragment.setArguments(bundle);
+        return crimeFragment;
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,6 +79,10 @@ public class CrimeFragment extends Fragment {
 
         crimeDetailViewModel.loadCrime(crimeID);
 
+
+       contactResultLauncher = registerForActivityResult(new ActivityResultContracts.PickContact(), result -> {
+            setSuspect(result);
+        });
     }
 
     @Nullable
@@ -69,6 +95,7 @@ public class CrimeFragment extends Fragment {
         solvedCheckBox = view.findViewById(R.id.crime_solved);
         requiresPoliceCheckBox = view.findViewById(R.id.requires_police);
         reportButton = view.findViewById(R.id.crime_report);
+        suspectButton = view.findViewById(R.id.crime_suspect);
 
         return view;
     }
@@ -119,8 +146,7 @@ public class CrimeFragment extends Fragment {
             if (isChecked) {
                 solvedCheckBox.setChecked(false);
             }
-        }
-        ));
+        }));
 
         dateButton.setOnClickListener(view -> {
             DatePickerFragment datePickerFragment = DatePickerFragment.newInstance(crime.getDate());
@@ -148,15 +174,19 @@ public class CrimeFragment extends Fragment {
             intent = Intent.createChooser(intent, getString(R.string.send_report));
             startActivity(intent);
         });
+
+        suspectButton.setOnClickListener(view -> {
+            contactResultLauncher.launch(null);
+        });
     }
+
+    //endregion
 
     @Override
     public void onStop() {
         super.onStop();
         crimeDetailViewModel.saveCrime(crime);
     }
-
-    //endregion
 
     void updateUI() {
         titleField.setText(crime.getTitle());
@@ -167,8 +197,28 @@ public class CrimeFragment extends Fragment {
         solvedCheckBox.setChecked(crime.isSolved());
         requiresPoliceCheckBox.setChecked(crime.requiresPolice);
 
+        if (crime.getSuspect() != null && crime.getSuspect() != "" ) {
+            suspectButton.setText(crime.getSuspect());
+        }
+
         solvedCheckBox.jumpDrawablesToCurrentState();
         requiresPoliceCheckBox.jumpDrawablesToCurrentState();
+    }
+
+    void setSuspect(Uri contactUri) {
+        Cursor cursor = requireActivity().getContentResolver().query(contactUri,
+                new String[]{ContactsContract.Contacts.DISPLAY_NAME},
+                null, null, null);
+        if (cursor.getCount() == 0) {
+            return;
+        }
+        cursor.moveToFirst();
+        String suspect = cursor.getString(0);
+        cursor.close();
+
+        crime.setSuspect(suspect);
+        crimeDetailViewModel.saveCrime(crime);
+        suspectButton.setText(suspect);
     }
 
     private String getCrimeReport(){
@@ -189,14 +239,6 @@ public class CrimeFragment extends Fragment {
         }
 
         return getString(R.string.crime_report, crime.getTitle(), dateString, solvedString, suspect);
-    }
-
-    public static CrimeFragment newInstance(UUID crimeID) {
-        Bundle bundle = new Bundle();
-        bundle.putSerializable(ARG_CRIME_ID, crimeID);
-        CrimeFragment crimeFragment = new CrimeFragment();
-        crimeFragment.setArguments(bundle);
-        return crimeFragment;
     }
 
 }
